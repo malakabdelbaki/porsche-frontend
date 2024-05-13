d3d68788dce9a724c3d833a1ba2a247b798e154c
toqa.amr <toqa.amr04@gmail.com>
2024-05-13 20:21:08 +0300

    test

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import axios from 'axios'
+import { useEffect, useContext , useState } from 'react'
+import {Link , useLocation} from 'react-router-dom'
+import './Home.css'
+
+const Home = () => {
+
+const {search} = useLocation()
+
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default Home
