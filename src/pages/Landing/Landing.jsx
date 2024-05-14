1a0ebf03673a613b1ebea092c75a1a0a07b326f8
MalakIsmail <119429621+malakabdelbaki@users.noreply.github.com>
2024-05-14 21:06:24 +0300

    bug fix on landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Background from './Background/Background';
 import Navbar from '../Navbar/Navbar';
-import Hero from './Hero/Hero';
 
 import './Landing.css'
 const Landing = () => {
