28bd18023eb3671e40c6e9178942d8965a0aca88
toqa.amr <toqa.amr04@gmail.com>
2024-05-15 01:38:02 +0300

    add home page logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter,Routes,Route } from "react-router-dom";
 import Admin from "./pages/Admin/Admin";
 import Landing from "./pages/Landing/Landing";
 import Cart from './pages/Cart/Cart'; 
+import Home from "./pages/Home/Home";
 
 function App() {
 
@@ -14,6 +15,8 @@ function App() {
           <Route path="/" element={<Landing />} />
           <Route path="/admin" element={<Admin />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/home" element={<Home />} />
+
         </Routes>
       </BrowserRouter>
     </div>
