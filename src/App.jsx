d526d05bbc6991b8a7619630cef52b6b7b5f9089
MalakIsmail <119429621+malakabdelbaki@users.noreply.github.com>
2024-05-16 14:37:55 +0300

    minor updates

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import { BrowserRouter,Routes,Route } from "react-router-dom";
 import Admin from "./pages/Admin/Admin";
 import Landing from "./pages/Landing/Landing";
 import Cart from './pages/Cart/Cart'; 
-
+import Product from './pages/Product/Product';
+// import Login from './pages/Login/Login';
+import Home from './pages/Home/Home';
+// import RegisterCustomer from './pages/RegisterCustomer/RegisterCustomer';
 function App() {
 
   return (
@@ -15,6 +18,10 @@ function App() {
           <Route path="/admin" element={<Admin />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/product/:id" element={<Product />} />
+          {/* <Route path="/login" element={<Login />} /> */}
+          <Route path="/home" element={<Home />} />
+          {/* <Route path="/register" element={<RegisterCustomer />} /> */}
+
         </Routes>
       </BrowserRouter>
     </div>
