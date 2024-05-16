64b135e5874c7aab0b4815ce5f87eebffa70482e
yousefnegmeldin <yousefnegm@gmail.com>
2024-05-16 14:18:53 +0300

    fixed url

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import React, { Component } from 'react';
+import { Component } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import "./Product.css";
