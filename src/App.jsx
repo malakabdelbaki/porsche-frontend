40db6582e12189740d8280022995fd62542d048c
phebeeee <phebe.ghattas@gmail.com>
2024-05-16 13:00:22 +0300

    product css

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter,Routes,Route } from "react-router-dom";
 import Admin from "./pages/Admin/Admin";
 import Landing from "./pages/Landing/Landing";
 import Cart from './pages/Cart/Cart'; 
+import Product from './pages/Product/Product';
 
 function App() {
 
